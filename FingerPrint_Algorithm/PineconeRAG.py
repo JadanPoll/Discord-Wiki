6a62caff23d9579421c344ce5a29fde2c5557216
ShubhanC <shubhan.chari@gmail.com>
2024-11-25 11:09:16 -0800

    pinecone RAG base

--- FingerPrint_Algorithm/PineconeRAG.py
+++ FingerPrint_Algorithm/PineconeRAG.py
@@ -1,3 +1,5 @@
+# pip install pinecone
+# DO NOT install pinecone-client
 from pinecone import Pinecone, ServerlessSpec
 import json
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -22,13 +24,14 @@ if not pc.has_index(index_name):
 file_path = '/Users/shubhan/Discord-Wiki/FingerPrint_Algorithm/DiscServers/ECE 120 Fall 2024 - exams - mt3 [1275789619954323534].json'
 index = pc.Index(index_name)
 
+'''
 tfidf_dict = {}
 with open('/Users/shubhan/Discord-Wiki/FingerPrint_Algorithm/Physics/Study Together - 💭 Community - general [595999872222756888] (after 2024-11-01)_tfidf.json', 'r', encoding='utf-8') as file:
     tfidf_dict = json.load(file)
 
 tokenizer = AutoTokenizer.from_pretrained("intfloat/e5-large")
 model = AutoModel.from_pretrained("intfloat/e5-base-v2")
-
+'''
 # Function to generate embeddings
 def generate_embedding(text):
     embeddings = pc.inference.embed("multilingual-e5-large",
--- FingerPrint_Algorithm/PineconeUpsert.py
+++ FingerPrint_Algorithm/PineconeUpsert.py
@@ -22,13 +22,6 @@ if not pc.has_index(index_name):
 file_path = '/Users/shubhan/Discord-Wiki/FingerPrint_Algorithm/DiscServers/ECE 120 Fall 2024 - exams - mt3 [1275789619954323534].json'
 index = pc.Index(index_name)
 
-tfidf_dict = {}
-with open('/Users/shubhan/Discord-Wiki/FingerPrint_Algorithm/Physics/Study Together - 💭 Community - general [595999872222756888] (after 2024-11-01)_tfidf.json', 'r', encoding='utf-8') as file:
-    tfidf_dict = json.load(file)
-
-tokenizer = AutoTokenizer.from_pretrained("intfloat/e5-large")
-model = AutoModel.from_pretrained("intfloat/e5-base-v2")
-
 # Function to generate embeddings
 def generate_embedding(text):
     embeddings = pc.inference.embed("multilingual-e5-large",
